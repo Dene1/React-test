a7e8d380001be81cde3daec10964e97ccc906fc3
Ilya Smit <ilya_smit@mail.ru>
2024-12-05 02:12:33 +0300

    Без JSX

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {createElement} from "react";
 
 function App() {
     // Императивный стиль
     const year = new Date().getFullYear()
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Happy hacking!
-                </p>
-                <h1>Current Year: {year}</h1>
-            </header>
-        </div>
-    );
+        createElement("div", {className: "App"},
+            createElement("header", {className: "App-header"},
+                createElement("img", {
+                    src: logo,
+                    className: "App-logo",
+                    alt: "logo"
+                }),
+                createElement("p", null, "Happy hacking!"),
+                createElement("h1", null, "Current Year: ", year))))
 }
 
 export default App;
