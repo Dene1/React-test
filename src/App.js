b6ba60b022224597d677af95b38a4b54b4bcd3b1
Ilya Smit <ilya_smit@mail.ru>
2024-12-05 01:56:18 +0300

    C JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Императивный стиль
+    const year = new Date().getFullYear()
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Happy hacking!
+                </p>
+                <h1>Current Year: {year}</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
